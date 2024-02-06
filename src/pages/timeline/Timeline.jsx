f90e3c7994c74ebcd175deeb26e5cc8a340e308a
Ritav Kashyap <ritav20-237@aec.ac.in>
2024-02-06 20:38:28 +0530

    timeline done

--- src/pages/timeline/Timeline.jsx
+++ src/pages/timeline/Timeline.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 import "./timeline.scss";
+import TimelineContent from "./TimelineContent";
+import { Link } from "react-router-dom";
 
 const Timeline = () => {
+  const {
+    year2024,
+    year2023,
+    year2022,
+    year2021,
+    year2020,
+    year2019,
+    year2018,
+  } = TimelineContent();
+
   return (
     <div className="Timeline">
       <img
@@ -10,6 +22,270 @@ const Timeline = () => {
         className="header-image"
       />
       <div className="header-text">Timeline</div>
+
+      <div
+        className="page"
+        data-uia-timeline-skin={4}
+        data-uia-timeline-adapter-skin-4="ui-card-skin-#1"
+      >
+        <div className="uia-timeline">
+          <div className="uia-timeline__container">
+            <div className="uia-timeline__line" />
+            <div className="uia-timeline__annual-sections">
+              <span className="uia-timeline__year" aria-hidden="true">
+                2024
+              </span>
+              <div className="uia-timeline__groups">
+                {year2024.map((year, index) => (
+                  <section
+                    className="uia-timeline__group"
+                    aria-labelledby="timeline-demo-4-heading-1"
+                    key={index}
+                  >
+                    <Link to={year.link} className="link">
+                      <div
+                        className="uia-timeline__point uia-card"
+                        data-uia-card-skin={1}
+                        data-uia-card-mod={1}
+                      >
+                        <div className="uia-card__container">
+                          <div className="uia-card__intro">
+                            <h3 className="ra-heading">{year.title}</h3>
+                            <span className="uia-card__time">
+                              <span className="uia-card__day">{year.date}</span>
+                            </span>
+                          </div>
+                          <div className="uia-card__body">
+                            <div className="uia-card__description">
+                              <p>{year.para}</p>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </Link>
+                  </section>
+                ))}
+              </div>
+            </div>
+            <div className="uia-timeline__annual-sections">
+              <span className="uia-timeline__year" aria-hidden="true">
+                2023
+              </span>
+              <div className="uia-timeline__groups">
+                {year2023.map((year, index) => (
+                  <section
+                    className="uia-timeline__group"
+                    aria-labelledby="timeline-demo-4-heading-1"
+                    key={index}
+                  >
+                    <Link to={year.link} className="link">
+                      <div
+                        className="uia-timeline__point uia-card"
+                        data-uia-card-skin={1}
+                        data-uia-card-mod={1}
+                      >
+                        <div className="uia-card__container">
+                          <div className="uia-card__intro">
+                            <h3 className="ra-heading">{year.title}</h3>
+                            <span className="uia-card__time">
+                              <span className="uia-card__day">{year.date}</span>
+                            </span>
+                          </div>
+                          <div className="uia-card__body">
+                            <div className="uia-card__description">
+                              <p>{year.para}</p>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </Link>
+                  </section>
+                ))}
+              </div>
+            </div>
+            <div className="uia-timeline__annual-sections">
+              <span className="uia-timeline__year" aria-hidden="true">
+                2022
+              </span>
+              <div className="uia-timeline__groups">
+                {year2022.map((year, index) => (
+                  <section
+                    className="uia-timeline__group"
+                    aria-labelledby="timeline-demo-4-heading-1"
+                    key={index}
+                  >
+                    <Link to={year.link} className="link">
+                      <div
+                        className="uia-timeline__point uia-card"
+                        data-uia-card-skin={1}
+                        data-uia-card-mod={1}
+                      >
+                        <div className="uia-card__container">
+                          <div className="uia-card__intro">
+                            <h3 className="ra-heading">{year.title}</h3>
+                            <span className="uia-card__time">
+                              <span className="uia-card__day">{year.date}</span>
+                            </span>
+                          </div>
+                          <div className="uia-card__body">
+                            <div className="uia-card__description">
+                              <p>{year.para}</p>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </Link>
+                  </section>
+                ))}
+              </div>
+            </div>
+            <div className="uia-timeline__annual-sections">
+              <span className="uia-timeline__year" aria-hidden="true">
+                2021
+              </span>
+              <div className="uia-timeline__groups">
+                {year2021.map((year, index) => (
+                  <section
+                    className="uia-timeline__group"
+                    aria-labelledby="timeline-demo-4-heading-1"
+                    key={index}
+                  >
+                    <Link to={year.link} className="link">
+                      <div
+                        className="uia-timeline__point uia-card"
+                        data-uia-card-skin={1}
+                        data-uia-card-mod={1}
+                      >
+                        <div className="uia-card__container">
+                          <div className="uia-card__intro">
+                            <h3 className="ra-heading">{year.title}</h3>
+                            <span className="uia-card__time">
+                              <span className="uia-card__day">{year.date}</span>
+                            </span>
+                          </div>
+                          <div className="uia-card__body">
+                            <div className="uia-card__description">
+                              <p>{year.para}</p>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </Link>
+                  </section>
+                ))}
+              </div>
+            </div>
+            <div className="uia-timeline__annual-sections">
+              <span className="uia-timeline__year" aria-hidden="true">
+                2020
+              </span>
+              <div className="uia-timeline__groups">
+                {year2020.map((year, index) => (
+                  <section
+                    className="uia-timeline__group"
+                    aria-labelledby="timeline-demo-4-heading-1"
+                    key={index}
+                  >
+                    <Link to={year.link} className="link">
+                      <div
+                        className="uia-timeline__point uia-card"
+                        data-uia-card-skin={1}
+                        data-uia-card-mod={1}
+                      >
+                        <div className="uia-card__container">
+                          <div className="uia-card__intro">
+                            <h3 className="ra-heading">{year.title}</h3>
+                            <span className="uia-card__time">
+                              <span className="uia-card__day">{year.date}</span>
+                            </span>
+                          </div>
+                          <div className="uia-card__body">
+                            <div className="uia-card__description">
+                              <p>{year.para}</p>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </Link>
+                  </section>
+                ))}
+              </div>
+            </div>
+            <div className="uia-timeline__annual-sections">
+              <span className="uia-timeline__year" aria-hidden="true">
+                2019
+              </span>
+              <div className="uia-timeline__groups">
+                {year2019.map((year, index) => (
+                  <section
+                    className="uia-timeline__group"
+                    aria-labelledby="timeline-demo-4-heading-1"
+                    key={index}
+                  >
+                    <Link to={year.link} className="link">
+                      <div
+                        className="uia-timeline__point uia-card"
+                        data-uia-card-skin={1}
+                        data-uia-card-mod={1}
+                      >
+                        <div className="uia-card__container">
+                          <div className="uia-card__intro">
+                            <h3 className="ra-heading">{year.title}</h3>
+                            <span className="uia-card__time">
+                              <span className="uia-card__day">{year.date}</span>
+                            </span>
+                          </div>
+                          <div className="uia-card__body">
+                            <div className="uia-card__description">
+                              <p>{year.para}</p>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </Link>
+                  </section>
+                ))}
+              </div>
+            </div>
+            <div className="uia-timeline__annual-sections">
+              <span className="uia-timeline__year" aria-hidden="true">
+                2018
+              </span>
+              <div className="uia-timeline__groups">
+                {year2018.map((year, index) => (
+                  <section
+                    className="uia-timeline__group"
+                    aria-labelledby="timeline-demo-4-heading-1"
+                    key={index}
+                  >
+                    <Link to={year.link} className="link">
+                      <div
+                        className="uia-timeline__point uia-card"
+                        data-uia-card-skin={1}
+                        data-uia-card-mod={1}
+                      >
+                        <div className="uia-card__container">
+                          <div className="uia-card__intro">
+                            <h3 className="ra-heading">{year.title}</h3>
+                            <span className="uia-card__time">
+                              <span className="uia-card__day">{year.date}</span>
+                            </span>
+                          </div>
+                          <div className="uia-card__body">
+                            <div className="uia-card__description">
+                              <p>{year.para}</p>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </Link>
+                  </section>
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
---
+++ src/pages/timeline/TimelineContent.jsx
@@ -0,0 +1,143 @@
+const TimelineContent = () => {
+  const year2024 = [
+    {
+      title: "The part of my life in University of Pennsylvania",
+      date: "2 Feb",
+      para: "Attends the Philadelphia Museum School of Industrial Art. Studies design with Alexey Brodovitch, art director at Harper's Bazaar, and works as his assistant.",
+      link: "/"
+    },
+    {
+      title: "The part of my life in University of Pennsylvania",
+      date: "2 Feb",
+      para: "Attends the Philadelphia Museum School of Industrial Art. Studies design with Alexey Brodovitch, art director at Harper's Bazaar, and works as his assistant.",
+      link: "/"
+    },
+  ];
+  const year2023 = [
+    {
+      title: "The part of my life in University of Pennsylvania",
+      date: "2 Feb",
+      para: "Attends the Philadelphia Museum School of Industrial Art. Studies design with Alexey Brodovitch, art director at Harper's Bazaar, and works as his assistant.",
+      link: "/"
+    },
+    {
+      title: "The part of my life in University of Pennsylvania",
+      date: "2 Feb",
+      para: "Attends the Philadelphia Museum School of Industrial Art. Studies design with Alexey Brodovitch, art director at Harper's Bazaar, and works as his assistant.",
+      link: "/"
+    },
+  ];
+  const year2022 = [
+    {
+      title: "The part of my life in University of Pennsylvania",
+      date: "2 Feb",
+      para: "Attends the Philadelphia Museum School of Industrial Art. Studies design with Alexey Brodovitch, art director at Harper's Bazaar, and works as his assistant.",
+      link: "/"
+    },
+    {
+      title: "The part of my life in University of Pennsylvania",
+      date: "2 Feb",
+      para: "Attends the Philadelphia Museum School of Industrial Art. Studies design with Alexey Brodovitch, art director at Harper's Bazaar, and works as his assistant.",
+      link: "/"
+    },
+    {
+      title: "The part of my life in University of Pennsylvania",
+      date: "2 Feb",
+      para: "Attends the Philadelphia Museum School of Industrial Art. Studies design with Alexey Brodovitch, art director at Harper's Bazaar, and works as his assistant.",
+      link: "/"
+    },
+  ];
+  const year2021 = [
+    {
+      title: "The part of my life in University of Pennsylvania",
+      date: "2 Feb",
+      para: "Attends the Philadelphia Museum School of Industrial Art. Studies design with Alexey Brodovitch, art director at Harper's Bazaar, and works as his assistant.",
+      link: "/"
+    },
+    {
+      title: "The part of my life in University of Pennsylvania",
+      date: "2 Feb",
+      para: "Attends the Philadelphia Museum School of Industrial Art. Studies design with Alexey Brodovitch, art director at Harper's Bazaar, and works as his assistant.",
+      link: "/"
+    },
+    {
+      title: "The part of my life in University of Pennsylvania",
+      date: "2 Feb",
+      para: "Attends the Philadelphia Museum School of Industrial Art. Studies design with Alexey Brodovitch, art director at Harper's Bazaar, and works as his assistant.",
+      link: "/"
+    },
+  ];
+  const year2020 = [
+    {
+      title: "The part of my life in University of Pennsylvania",
+      date: "2 Feb",
+      para: "Attends the Philadelphia Museum School of Industrial Art. Studies design with Alexey Brodovitch, art director at Harper's Bazaar, and works as his assistant.",
+      link: "/"
+    },
+    {
+      title: "The part of my life in University of Pennsylvania",
+      date: "2 Feb",
+      para: "Attends the Philadelphia Museum School of Industrial Art. Studies design with Alexey Brodovitch, art director at Harper's Bazaar, and works as his assistant.",
+      link: "/"
+    },
+    {
+      title: "The part of my life in University of Pennsylvania",
+      date: "2 Feb",
+      para: "Attends the Philadelphia Museum School of Industrial Art. Studies design with Alexey Brodovitch, art director at Harper's Bazaar, and works as his assistant.",
+      link: "/"
+    },
+  ];
+  const year2019 = [
+    {
+      title: "The part of my life in University of Pennsylvania",
+      date: "2 Feb",
+      para: "Attends the Philadelphia Museum School of Industrial Art. Studies design with Alexey Brodovitch, art director at Harper's Bazaar, and works as his assistant.",
+      link: "/"
+    },
+    {
+      title: "The part of my life in University of Pennsylvania",
+      date: "2 Feb",
+      para: "Attends the Philadelphia Museum School of Industrial Art. Studies design with Alexey Brodovitch, art director at Harper's Bazaar, and works as his assistant.",
+      link: "/"
+    },
+    {
+      title: "The part of my life in University of Pennsylvania",
+      date: "2 Feb",
+      para: "Attends the Philadelphia Museum School of Industrial Art. Studies design with Alexey Brodovitch, art director at Harper's Bazaar, and works as his assistant.",
+      link: "/"
+    },
+  ];
+  const year2018 = [
+    {
+      title: "The part of my life in University of Pennsylvania",
+      date: "2 Feb",
+      para: "Attends the Philadelphia Museum School of Industrial Art. Studies design with Alexey Brodovitch, art director at Harper's Bazaar, and works as his assistant.",
+      link: "/"
+    },
+    {
+      title: "The part of my life in University of Pennsylvania",
+      date: "2 Feb",
+      para: "Attends the Philadelphia Museum School of Industrial Art. Studies design with Alexey Brodovitch, art director at Harper's Bazaar, and works as his assistant.",
+      link: "/"
+    },
+    {
+      title: "The part of my life in University of Pennsylvania",
+      date: "2 Feb",
+      para: "Attends the Philadelphia Museum School of Industrial Art. Studies design with Alexey Brodovitch, art director at Harper's Bazaar, and works as his assistant.",
+      link: "/"
+    },
+  ];
+  
+  // Return the object directly
+  return {
+    year2024,
+    year2023,
+    year2022,
+    year2021,
+    year2020,
+    year2019,
+    year2018,
+  };
+};
+
+export default TimelineContent;
