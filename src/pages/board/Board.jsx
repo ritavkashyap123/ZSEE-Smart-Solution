6a1d58c00aadb64e47cfcc2534e63bd9e3253340
Ritav Kashyap <ritav20-237@aec.ac.in>
2024-02-12 02:23:50 +0530

    board member update

--- src/pages/board/Board.jsx
+++ src/pages/board/Board.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import "./board.scss";
+import { Link } from "react-router-dom";
 
 import { FaTwitter, FaFacebook, FaInstagram, FaYoutube } from "react-icons/fa";
-
+import BoardContent from "./BoardContent";
 
 const Board = () => {
+  const { chairPerson, boardMembers } = BoardContent();
+
   return (
     <div className="Board">
       <img
@@ -14,33 +17,60 @@ const Board = () => {
       />
       <div className="header-text">Board Members</div>
       <div className="container">
+        <h1 className="head">Chairperson</h1> 
         <div className="card_container">
-          <div className="card_data">
-            <img src="./profile.jpg" alt="" />
-            <div className="pro_content">
-              <h2>Walter White</h2>
-              <p className="position">Chief Executive Officer</p>
-              <p>Explicabo voluptatem mollitia et repellat qui dolorum quasi</p>
-              <div className="social_icons">
-                <span>
-                  {/* <i className="fa fa-brands fa-twitter" /> */}
-                  <FaTwitter className="fa" />
-                </span>
-                <span>
-                  {/* <i className="fa fa-brands fa-facebook" /> */}
-                  <FaFacebook className="fa" />
-                </span>
-                <span>
-                  {/* <i className="fa fa-brands fa-youtube" /> */}
-                  <FaInstagram className="fa" />
-                </span>
-                <span>
-                  {/* <i className="fa fa-brands fa-instagram" /> */}
-                  <FaYoutube className="fa" />
-                </span>
+          {chairPerson.map((chair, index) => (
+            <div className="card_data" key={index}>
+              <img src={chair.image} alt="" />
+              <div className="pro_content">
+                <h2>{chair.title}</h2>
+                <p className="position">{chair.position}</p>
+                <p>{chair.description}</p>
+                <div className="social_icons">
+                  
+                  <Link to={chair.tweeterlink} className="span">
+                    <FaTwitter className="fa" />
+                  </Link>
+                  <Link to={chair.fblink} className="span">
+                    <FaFacebook className="fa" />
+                  </Link>
+                  <Link to={chair.instalink} className="span">
+                    <FaInstagram className="fa" />
+                  </Link>
+                  <Link to={chair.ytlink} className="span">
+                    <FaYoutube className="fa" />
+                  </Link>
+                </div>
               </div>
             </div>
-          </div>
+          ))}
+        </div>
+        <h1 className="head">Board Members</h1>
+        <div className="card_container">
+          {boardMembers.map((board, index) => (
+            <div className="card_data" key={index}>
+              <img src={board.image} alt="" />
+              <div className="pro_content">
+                <h2>{board.title}</h2>
+                <p className="position">{board.position}</p>
+                <p>{board.description}</p>
+                <div className="social_icons">
+                  <Link to={board.tweeterlink} className="span">
+                    <FaTwitter className="fa" />
+                  </Link>
+                  <Link to={board.fblink} className="span">
+                    <FaFacebook className="fa" />
+                  </Link>
+                  <Link to={board.instalink} className="span">
+                    <FaInstagram className="fa" />
+                  </Link>
+                  <Link to={board.ytlink} className="span">
+                    <FaYoutube className="fa" />
+                  </Link>
+                </div>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </div>
---
+++ src/pages/board/BoardContent.jsx
@@ -0,0 +1,67 @@
+const BoardContent = () => {
+  const chairPerson = [
+    {
+      title: "Walter White",
+      position: "Chief Executive Officer",
+      description:
+        "Explicabo voluptatem mollitia et repellat qui dolorum quasi",
+      image: "./profile.jpg",
+      fblink: "",
+      instalink: "/",
+      tweeterlink: "/",
+      ytlink: "/",
+    },
+  ];
+  const boardMembers = [
+    {
+      title: "Walter White",
+      position: "Chief Executive Officer",
+      description:
+        "Explicabo voluptatem mollitia et repellat qui dolorum quasi",
+      image: "./profile.jpg",
+      fblink: "/",
+      instalink: "/",
+      tweeterlink: "/",
+      ytlink: "/",
+    },
+    {
+      title: "Walter White",
+      position: "Chief Executive Officer",
+      description:
+        "Explicabo voluptatem mollitia et repellat qui dolorum quasi",
+      image: "./profile.jpg",
+      fblink: "/",
+      instalink: "/",
+      tweeterlink: "/",
+      ytlink: "/",
+    },
+    {
+      title: "Walter White",
+      position: "Chief Executive Officer",
+      description:
+        "Explicabo voluptatem mollitia et repellat qui dolorum quasi",
+      image: "./profile.jpg",
+      fblink: "/",
+      instalink: "/",
+      tweeterlink: "/",
+      ytlink: "/",
+    },
+    {
+      title: "Walter White",
+      position: "Chief Executive Officer",
+      description:
+        "Explicabo voluptatem mollitia et repellat qui dolorum quasi",
+      image: "./profile.jpg",
+      fblink: "/",
+      instalink: "/",
+      tweeterlink: "/",
+      ytlink: "/",
+    },
+  ];
+  return {
+    chairPerson,
+    boardMembers,
+  };
+};
+
+export default BoardContent;
